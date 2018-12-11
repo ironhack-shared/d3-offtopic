a43c1bd5ff7764e7a4a2fd95b750c07864a7e19f
daniel vicario <danigv.email@gmail.com>
2018-12-11 11:05:58 +0100

    added initial setup plus app.js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,29 @@
-d3
-    .select("#base")
-    .append("circle")
-    .attr("r", 20)
+let App = {
+    colors: ['#ffcc00', '#000'],
+    _setFullScreen: function () {
+        d3
+            .select("#base")
+            .attr("width", window.innerWidth)
+            .attr("height", window.innerHeight)
+            .style("background-color", "#f00")
+    },
+    _setBackgroundColor: function (hasTransition) {
+        let selection = d3
+            .select("#base");
+
+        if (hasTransition) {
+            selection
+                .transition()
+                .duration(3000)
+                .style("background-color", this.colors[1])
+        }   else {
+            selection
+                .style("background-color", this.colors[1])
+        }
+
+    },
+    init: function () {
+        this._setFullScreen()
+        this._setBackgroundColor(false)
+    }
+}
